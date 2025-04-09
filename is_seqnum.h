d72cee9a175575c84ca2ee325c42f71b6311c3da
BenchKo <Benjaminkiewert@gmail.com>
2025-04-08 23:04:52 -0700

    Super

--- is_seqnum.h
+++ is_seqnum.h
@@ -3,7 +3,7 @@
 #include <sys/socket.h>
 #include "read_line.h"
 #include "tlpi_hdr.h"
-
+#include "error_functions.h"
 #define PORT_NUM                "50000"
 
 #define INT_LEN                 30  /* size of string able to hold largest Integer*/
